b4b10b4bc21d1b3b4dc95ede949005419142906b
xswang <2012wxs@gmail.com>
2018-03-12 19:49:57 +0800

    update: random

--- demo/random.cc
+++ demo/random.cc
@@ -10,6 +10,7 @@
 
 int main() {
   for (int i = 0; i < 10; ++i) {
+    std::cout << " === " << local_random_engine() << std::endl;
     auto val = local_normal_real_distribution<double>(0.0, 1.0)(local_random_engine());
     std::cout << val << std::endl;
   }
