cf456f124bbf14ece913917e9fedc3778d1d7dce
xswang <2012wxs@gmail.com>
2018-03-12 19:29:24 +0800

    add: random normal

---
+++ demo/random.cc
@@ -0,0 +1,19 @@
+/*
+ * random.h
+ * Copyright (C) 2018 XiaoshuWang <2012wxs@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+
+#include "/root/xiaoshu/opensource/code-base/demo/random.h"
+
+int main() {
+  for (int i = 0; i < 10; ++i) {
+    auto val = local_normal_real_distribution<double>(0.0, 1.0)(local_random_engine());
+    std::cout << val << std::endl;
+  }
+
+  return 0;
+}
+
