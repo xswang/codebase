f8ec38b16bf840f0eebb98f1fee4e71b3337a954
xswang <2012wxs@gmail.com>
2018-03-16 12:24:05 +0800

    add memory

---
+++ demo/memory.cc
@@ -0,0 +1,12 @@
+/*
+ * memory.cc
+ * Copyright (C) 2018 XiaoshuWang <2012wxs@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+#include <iostream>
+
+int main() {
+  int mydata[3 * 4];
+  std::cout << sizeof(mydata) << std::endl;
+}
